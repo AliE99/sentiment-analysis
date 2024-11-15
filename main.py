92cafa3b2d5534fdcd95500cdeb86018d90e9e23
Ali Elahi <alielahi.1378@gmail.com>
2024-11-15 11:42:04 +0330

    Add function to remove stopwords from text using NLTK

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import re
 import string
-import pandas as pd
+
 import nltk
+import pandas as pd
+from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 nltk.download("punkt")
+nltk.download("stopwords")
 
 
 class SentimentAnalysis:
     def __init__(self, file_path: str = "data/sentiment.csv"):
         self.data = pd.read_csv(file_path, on_bad_lines="skip", engine="python")
+        self.stop_words: set = set(stopwords.words("english"))
 
     def pre_process(self):
         self.make_all_tweets_lowercase()
@@ -17,13 +21,14 @@ class SentimentAnalysis:
         self.data["text"] = self.data["text"].apply(self.remove_punctuation)
         self.data["text"] = self.data["text"].apply(self.remove_emojis)
         self.data["text"] = self.data["text"].apply(self.tokenize)
+        self.data["text"] = self.data["text"].apply(self.remove_stopwords)
 
         print(self.data.head(10))
 
     def make_all_tweets_lowercase(self):
         """
         Make all text lowercase in the 'text' column
-        :return:
+        :return: list of all text lowercase
         """
         self.data["text"] = self.data["text"].str.lower()
 
@@ -68,11 +73,28 @@ class SentimentAnalysis:
         return emoji_pattern.sub("", text)
 
     def tokenize(self, text):
+        """
+        Tokenize a string.
+        :param text: The input string.
+        :return: tokenized string.
+        """
         if not isinstance(text, str):
             return text
 
         tokens = word_tokenize(text)
         return tokens
 
+    def remove_stopwords(self, text):
+        """
+        Remove stopwords from a string.
+        :param text: The input string.
+        :return: filtered string.
+        """
+        if not isinstance(text, str):
+            return text
 
-SentimentAnalysis().pre_process()
+        filtered_words = [word for word in text if word.lower() not in self.stop_words]
+        return filtered_words
+
+
+SentimentAnalysis(file_path="data/sentiment.csv").pre_process()
