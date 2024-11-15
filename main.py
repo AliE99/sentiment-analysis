e63125092740a9aa21ca8a90d304b5d4b9b4f9b0
Ali Elahi <alielahi.1378@gmail.com>
2024-11-15 10:56:21 +0330

    Add tokenization functions for text preprocessing using nltk library

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 import re
 import string
 import pandas as pd
+import nltk
+from nltk.tokenize import word_tokenize
+
+nltk.download("punkt")
 
 
 class SentimentAnalysis:
     def __init__(self, file_path: str = "data/sentiment.csv"):
         self.data = pd.read_csv(file_path, on_bad_lines="skip", engine="python")
-        print(self.data.head(10))
 
     def pre_process(self):
         self.make_all_tweets_lowercase()
         self.data["text"] = self.data["text"].apply(self.remove_url_mentions_hashtags)
         self.data["text"] = self.data["text"].apply(self.remove_punctuation)
         self.data["text"] = self.data["text"].apply(self.remove_emojis)
+        self.data["text"] = self.data["text"].apply(self.tokenize)
 
-        print(self.data.head(100))
+        print(self.data.head(10))
 
     def make_all_tweets_lowercase(self):
         """
@@ -62,6 +66,13 @@ class SentimentAnalysis:
 
         emoji_pattern = re.compile("[\U00010000-\U0010FFFF]", flags=re.UNICODE)
         return emoji_pattern.sub("", text)
+
+    def tokenize(self, text):
+        if not isinstance(text, str):
+            return text
+
+        tokens = word_tokenize(text)
+        return tokens
 
 
 SentimentAnalysis().pre_process()
